6a356b59cf89d0b611324d1fa712b04ec3517581
Burhan Aras <burhanaras@live.com>
2023-01-17 17:04:43 +0300

    feat(tableView): fetch data from json file

---
+++ Project 03 - TableView - CollectionView/TableView - CollectionView/Models/ProductsModel.swift
@@ -0,0 +1,27 @@
+//
+//  ProductsModel.swift
+//  TableView - CollectionView
+//
+//  Created by Burhan Aras on 17.01.2023.
+//
+
+import Foundation
+
+struct ProductModel: Codable {
+
+    let response: [Product]?
+}
+
+struct Product: Codable {
+
+    let categoryName: String?
+    let products: [ProductDetails]?
+}
+
+struct ProductDetails: Codable {
+
+    let name: String?
+    let imageName: String?
+    let price: String?
+    let description: String?
+}
--- Project 03 - TableView - CollectionView/TableView - CollectionView/ViewController.swift
+++ Project 03 - TableView - CollectionView/TableView - CollectionView/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    var productData: ProductModel?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadJson()
     }
 
+    private func loadJson() {
+        if let path = Bundle.main.path(forResource: "JsonData", ofType: ".json") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+                let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
+                let jsonData = try JSONSerialization.data(withJSONObject: jsonResult, options: .prettyPrinted)
+                let jsonDecoder = JSONDecoder()
+                jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+                productData = try jsonDecoder.decode(ProductModel.self, from: jsonData)
+                print(productData?.response?.count ?? -1)
+            }
+            catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
 
 }
 
